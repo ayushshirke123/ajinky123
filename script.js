087e3e716d9df8c39d21d42c2f9bae8065e2f13e
ayushshirke123 <152913201+ayushshirke123@users.noreply.github.com>
2025-03-01 18:15:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+        function toggleDetails(id) {
+            let details = document.getElementById(id);
+            details.style.display = details.style.display === 'block' ? 'none' : 'block';
+        }
+    
